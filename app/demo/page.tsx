df6969e5f79a51bb983ff3c1fbbc4536289668ef
taratap2004 <taralnw226059@gmail.com>
2025-02-11 11:07:56 +0700

    update

---
+++ app/demo/page.tsx
@@ -0,0 +1,12 @@
+import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert"
+import { Terminal } from "lucide-react"
+
+
+
+<Alert>
+  <Terminal className="h-4 w-4" />
+  <AlertTitle>Heads up!</AlertTitle>
+  <AlertDescription>
+    You can add components and dependencies to your app using the cli.
+  </AlertDescription>
+</Alert>
